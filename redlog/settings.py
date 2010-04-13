c7afd716636ef73a89315d613e7b768f75245b3a
Oleksandr Lobunets <alexander.lobunets@gmail.com>
2010-04-13 11:04:50 +0200

    Updated README with missing activity setup

--- redlog/settings.py
+++ redlog/settings.py
@@ -6,5 +6,5 @@ REDLOG_VERSION = u'0.1'
 TICKING_INTERVAL = 5000
 
 # Redmine settings
-REDMINE_BASE_URL = u'http://my-redmine-installation'
+REDMINE_BASE_URL = u'http://https://redmine.postindustria.com:3443'
 REDMINE_ACTIVITIES = {'Mixed activity': '14'}
