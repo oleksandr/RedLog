05b71489e66f1637d54ccb0a0d0e6a4197e6f4fb
askalyuk <scater@pleso.net>
2010-04-15 20:09:53 +0300

    command line support improvement

--- redlog/export/export_xls.py
+++ redlog/export/export_xls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from redlog.models import RemoteIssuesStore
+from redlog.models import RemoteIssuesStore, LocalStore
 from redlog import settings
 import xlwt
 import logging
@@ -14,6 +14,7 @@ class XlsExporter:
         self.output_file = output_file
     
     def export(self):
+        logging.debug('Parsing issues...')
         wb = xlwt.Workbook(encoding='utf-8')
         ws = wb.add_sheet('estimation')
         
@@ -59,19 +60,20 @@ class XlsExporter:
         # add summary
         ws.write(len(self.issues) + 1, 8, xlwt.Formula("SUM(I2:I%s)" % (len(self.issues) + 1)))
         
+        logging.debug('Export to Excel file %s' % self.output_file)
         wb.save(self.output_file)
 
 def start(argv):
     try:                          
-        opts, args = getopt.getopt(argv, "", ["query_id="])
+        opts, args = getopt.getopt(argv, "", ["file=", "query_id="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
         
-    if len(opts) == 0:
+    if len(opts) < 2:
         usage()
         sys.exit(2)
-        
+      
     opt, arg = opts[0]
     if opt == "--query_id":
         query_id = arg
@@ -79,19 +81,38 @@ def start(argv):
         usage()
         sys.exit(2)
     
+    opt, arg = opts[1]   
+    if opt == "--file":
+        export_file = arg
+    else:
+        usage()
+        sys.exit(2)
+    
     logging.debug('XLS export process started (query_id = %s)' % query_id)
-    username = 'username'
-    password = 'password'
+    
+    localStore = LocalStore()
+    credentials = localStore.get_credentials()
+    
+    if len(credentials) == 0:
+        print "Can't find credentials for Redmine login. Use Redlog UI before."
+        
+    username = credentials[0]
+    password = credentials[1]
     logging.debug('Connect to Redmine with username "%s"' % username)
     remote_store = RemoteIssuesStore(settings.REDMINE_BASE_URL, username, password)
     
+    logging.debug('Getting issues...')
     issues = remote_store.get_issues_by_query_id(query_id) #41
-    xls_exporter = XlsExporter(issues, "c:\\export.xls")
+    
+    xls_exporter = XlsExporter(issues, export_file)
     xls_exporter.export()
     
+    logging.debug('Done')
+    
 def usage():
-    print "usage: export_xls --query_id=QUERY_ID"
-    print "QUERY_ID is id of saved query in Redmine"
+    print "usage: export_xls --query_id=QUERY_ID --file=FILE_TO_EXPORT"
+    print "QUERY_ID is id of saved query in Redmine. For example: 41"
+    print "FILE_TO_EXPORT is full path to export file. For example: c:\export.xls"
 
 if __name__ == '__main__':
     start(sys.argv[1:])
